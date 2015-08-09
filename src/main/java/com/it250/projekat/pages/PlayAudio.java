39afdf80ebba4c6dce34cb8eac2dfe09acc1402e
Workbench <Workbench@DESKTOP-P4BRI3L>
2015-08-09 13:23:54 +0200

    Merge origin/master
    Conflicts:
    	build.gradle
    	gradle/wrapper/gradle-wrapper.jar
    	gradle/wrapper/gradle-wrapper.properties
    	pom.xml
    	src/main/java/com/it250/projekat/components/Layout.java
    	src/main/java/com/it250/projekat/pages/About.java
    	src/main/java/com/it250/projekat/pages/Index.java
    	src/main/java/com/it250/projekat/pages/Login.java
    	src/main/java/com/it250/projekat/services/AppModule.java
    	src/main/resources/com/it250/projekat/components/Layout.tml
    	src/main/resources/com/it250/projekat/pages/About.tml
    	src/main/resources/com/it250/projekat/pages/Contact.tml
    	src/main/resources/com/it250/projekat/pages/Error404.tml
    	src/main/resources/com/it250/projekat/pages/Index.properties
    	src/main/resources/com/it250/projekat/pages/Index.tml
    	src/main/resources/com/it250/projekat/pages/Login.tml
    	src/main/resources/log4j.properties
    	src/main/webapp/META-INF/context.xml
    	src/main/webapp/WEB-INF/app.properties
    	src/main/webapp/WEB-INF/web.xml
    	src/main/webapp/favicon.ico
    	src/main/webapp/mybootstrap/css/bootstrap-theme.css
    	src/main/webapp/mybootstrap/css/bootstrap.css
    	src/main/webapp/mybootstrap/fonts/glyphicons-halflings-regular.eot
    	src/main/webapp/mybootstrap/fonts/glyphicons-halflings-regular.svg
    	src/main/webapp/mybootstrap/fonts/glyphicons-halflings-regular.ttf
    	src/main/webapp/mybootstrap/fonts/glyphicons-halflings-regular.woff
    	src/main/webapp/mybootstrap/fonts/glyphicons-halflings-regular.woff2
    	src/site/apt/index.apt
    	src/site/site.xml

--- src/main/java/com/it250/projekat/pages/PlayAudio.java
+++ src/main/java/com/it250/projekat/pages/PlayAudio.java
@@ -10,38 +10,82 @@ import com.it250.projekat.entities.Comment;
 import com.it250.projekat.entities.Song;
 import com.it250.projekat.entities.User;
 import java.util.ArrayList;
+import org.apache.tapestry5.annotations.ActivationRequestParameter;
+import org.apache.tapestry5.annotations.InjectComponent;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.beaneditor.Validate;
+import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 
 /**
  *
  * @author Workbench
  */
 public class PlayAudio {
+
     @Property
     @SessionState
     private User user;
-    
-    @Property
+
+    @Property @Persist
     private ArrayList<Comment> comments;
-    
+
     @Inject
     private CommentDao dao;
-    
+
     @Property
     private Comment comment;
-    
+
+    @InjectComponent
+    private Zone commentzone;
+
+    @Inject
+    private AjaxResponseRenderer ajax;
+
+    @Property
+    @Validate("required")
+    private String commentText;
+
+    @Inject
+    private Request request;
+
     @Property
     private Song song;
-    
-    void onActivate(Song s){
+
+    void onActivate(Song s) {
         song = s;
-        
-        if(comments == null){
+    }
+
+    void setupRender() {
+        System.out.println(song.getName());
+        if (comments == null) {
             comments = new ArrayList<Comment>();
         }
-        
         comments = (ArrayList<Comment>) dao.findComment(song);
+        ajax.addRender(commentzone);
     }
+
+    @CommitAfter
+    void onSuccessFromForm() {
+        System.out.println(song.getName());
+        Comment c = new Comment();
+        c.setSongId(song);
+        c.setUserId(user);
+        c.setText(commentText);
+        dao.add(c);
+        if (request.isXHR()) {
+            if (comments == null) {
+                comments = new ArrayList<Comment>();
+            }
+            comments = (ArrayList<Comment>) dao.findComment(song);
+            ajax.addRender(commentzone);
+        }
+    }
+    
+    
 }
